81a098226552beb7a43621903af727b1297db2f1
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-03-11 16:19:26 +0530

    correct link

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,6 @@
-export const host='http://localhost:5000/'
+const dotenv=require("dotenv").config();
+
+export const host=process.env.SERVER_URL;
 export const setAvatarRoute=`${host}api/auth/setAvatar`;
 export const loginRoute=`${host}api/auth/login`;
 export const registerRoute=`${host}api/auth/signup`;
