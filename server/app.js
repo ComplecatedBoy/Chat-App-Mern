0e24e89e238dec656f38e269f3571d5ab05f46ef
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-03-13 10:20:38 +0530

    setHeaders for cors

--- server/app.js
+++ server/app.js
@@ -19,6 +19,13 @@ const main=async ()=>{
 main();
 
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json());
 
 app.use("/api/auth",authRouter);
